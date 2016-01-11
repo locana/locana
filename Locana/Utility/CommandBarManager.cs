c6b3807649932c286585759b34af8f75e104699c
Nao Taco <naotaco@gmail.com>
2016-01-11 17:48:14 +0900

    Not to show label on Cancel touch AF button

--- Locana/Utility/CommandBarManager.cs
+++ Locana/Utility/CommandBarManager.cs
@@ -129,7 +129,7 @@ namespace Locana.Utility
                 case AppBarItem.AppSetting:
                     return new AppBarButton() { Icon = new SymbolIcon(Symbol.Setting), Label = SystemUtil.GetStringResource("AppSetting") };
                 case AppBarItem.CancelTouchAF:
-                    return new AppBarButton() { Icon = new SymbolIcon(Symbol.Cancel), Label = SystemUtil.GetStringResource("AppBar_CancelTouchAf") };
+                    return new AppBarButton() { ContentTemplate = (DataTemplate)Application.Current.Resources["CancelIcon"], Label = "" };
                 case AppBarItem.Close:
                     return new AppBarButton() { Icon = new SymbolIcon(Symbol.Cancel), Label = SystemUtil.GetStringResource("AppBar_Close") };
                 case AppBarItem.DeleteMultiple:
